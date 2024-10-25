9b57f269e9b18a7baa2308fe1ba1929c3e17da41
nilssen98 <kasper.nilssen1@gmail.com>
2024-10-25 13:21:07 +0200

    configure alacritty with dotfiles

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -2,10 +2,6 @@
 {
   home.packages = with pkgs; [ alacritty ];
 
-  programs.alacritty = {
-    enable = true;
-  };
-
   home.file."${config.home.homeDirectory}/.config/alacritty/alacritty.toml" = {
     source = ../../dotfiles/alacritty.toml;
   };
