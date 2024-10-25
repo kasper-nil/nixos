31b92ba121dc4bd3d945aee8a1281fbf82ec3c1d
nilssen98 <kasper.nilssen1@gmail.com>
2024-10-25 13:16:53 +0200

    add alacritty dotfile

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -1,12 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [ alacritty ];
 
   programs.alacritty = {
     enable = true;
-    settings = ''
-      [font]
-      size = 12
-    '';
+  };
+
+  home.file."${config.home.homeDirectory}/.config/alacritty/alacritty.toml" = {
+    source = ../../dotfiles/alacritty.toml;
   };
 }
