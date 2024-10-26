504541fe8fc52c2770d43bb042d615e8a78db896
nilssen98 <kasper.nilssen1@gmail.com>
2024-10-26 21:55:56 +0200

    add xfce and kde together

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -1,20 +1,39 @@
+{ pkgs, ... }:
 {
   # Options for all of the graphical aspects of the OS
   # Configures display manager, window manager and desktop manager
-
-  # kde + i3 setup
   services = {
+    displayManager = {
+      defaultSession = "xfce+i3";
+    };
+
+    desktopManager = {
+      plasma6.enable = true;
+    };
+
     xserver = {
       enable = true;
+
       windowManager = {
         i3.enable = true;
       };
-    };
-    displayManager = {
-      defaultSession = "plasma6+i3";
-    };
-    desktopManager = {
-      plasma6.enable = true;
+
+      desktopManager = {
+        xterm.enable = false;
+
+        xfce = {
+          enable = true;
+          noDesktop = true;
+          enableXfwm = false;
+        };
+
+        session = [
+          {
+            name = "plasma6+i3";
+            start = ''exec env KDEWM=${pkgs.i3-gaps}/bin/i3 ${pkgs.plasma-workspace}/bin/startplasma-x11'';
+          }
+        ];
+      };
     };
   };
 
