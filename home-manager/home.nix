81b1d4c1d45e32d4eb39c0bd965b1d5028e265b8
nilssen98 <kasper.nilssen1@gmail.com>
2024-10-26 22:36:39 +0200

    remove unused module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,7 +6,6 @@
     ../modules/home-manager/alacritty.nix
     ../modules/home-manager/i3status-rust.nix
     ../modules/home-manager/rofi.nix
-    ../modules/home-manager/plasma.nix
   ];
 
   home = {
