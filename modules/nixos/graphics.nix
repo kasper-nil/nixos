cd14f9f71bffddabfaf800b7c66f5cb77c3939e0
nilssen98 <kasper.nilssen1@gmail.com>
2024-10-26 22:36:14 +0200

    add a custom systemd service

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -1,23 +1,29 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  systemd.user.services.plasma-i3 = {
-    enable = true;
-  };
+  systemd.services.plasma-kwin_x11.wantedBy = lib.mkForce [ ];
 
-  systemd.user.services.plasma-kwin_x11 = {
-    enable = false;
+  systemd.services.plasma-i3 = {
+    enable = true;
+    description = "Plasma custom window manager";
+    wantedBy = [ "plasma-workspace.target" ];
+    before = [ "plasma-workspace.target" ];
+    serviceConfig = {
+      ExecStart = ''/usr/bin/i3'';
+      Slice = "session.slice";
+      Restart = "on-failure";
+    };
   };
 
   # Options for all of the graphical aspects of the OS
   # Configures display manager, window manager and desktop manager
   services = {
     displayManager = {
-      defaultSession = "xfce+i3";
+      defaultSession = "none+i3";
       sddm.enable = true;
     };
 
     desktopManager = {
-      plasma6.enable = true;
+      # plasma6.enable = true;
     };
 
     xserver = {
@@ -37,13 +43,6 @@
           noDesktop = true;
           enableXfwm = false;
         };
-
-        session = [
-          {
-            name = "plasma6+i3";
-            start = ''exec env KDEWM=${pkgs.i3-gaps}/bin/i3 ${pkgs.plasma-workspace}/bin/startplasma-x11'';
-          }
-        ];
       };
     };
   };
