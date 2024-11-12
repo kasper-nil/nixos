3b24331c4da8788a30b2fd149ac352a0db6aff74
nilssen98 <kasper.nilssen1@gmail.com>
2024-11-12 16:21:18 +0100

    polybar configuration

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -3,6 +3,8 @@
 # Terminate already running bar instances
 polybar-msg cmd quit
 
+polybar main &
+
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
--- modules/home-manager/polybar.nix
+++ modules/home-manager/polybar.nix
@@ -1,31 +1,22 @@
 { pkgs, config, ... }:
 {
-  home.file = {
-    # "${config.home.homeDirectory}/.config/polybar/launch.sh" = {
-    #   source = ../../dotfiles/polybar/launch.sh;
-    #   executable = true;
-    # };
+  home = {
+    packages = with pkgs; [
+      (polybar.override {
+        i3Support = true;
+        alsaSupport = true;
+        iwSupport = true;
+        githubSupport = true;
+        pulseSupport = true;
+      })
+    ];
 
-    "${config.home.homeDirectory}/.config/polybar" = {
-      source = ../../dotfiles/polybar;
-      recursive = true;
-      executable = true;
-    };
-
-    #"${config.home.homeDirectory}/.config/polybar/config.ini" = {
-    #  source = ../../dotfiles/polybar/config.ini;
-    #};
-    #
-  };
-
-  services.polybar = {
-    enable = true;
-    script = "";
-    package = pkgs.polybar.override {
-      i3Support = true;
-      alsaSupport = true;
-      iwSupport = true;
-      githubSupport = true;
+    file = {
+      "${config.home.homeDirectory}/.config/polybar" = {
+        source = ../../dotfiles/polybar;
+        recursive = true;
+        executable = true;
+      };
     };
   };
 }
