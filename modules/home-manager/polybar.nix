eedc73d576c79697ab9b42f7133a6cd3a30712af
nilssen98 <kasper.nilssen1@gmail.com>
2024-11-12 13:24:54 +0100

    add polybar configuration

--- modules/home-manager/polybar.nix
+++ modules/home-manager/polybar.nix
@@ -2,7 +2,12 @@
 {
   home.packages = with pkgs; [ polybar ];
 
-  # home.file."${config.home.homeDirectory}/.config/alacritty/alacritty.toml" = {
-  #   source = ../../dotfiles/alacritty.toml;
-  # };
+  home.file."${config.home.homeDirectory}/.config/polybar/config.ini" = {
+    source = ../../dotfiles/polybar/config.ini;
+  };
+
+  home.file."${config.home.homeDirectory}/.config/polybar/launch.sh" = {
+    source = ../../dotfiles/polybar/launch.sh;
+    executable = true;
+  };
 }
