352eb30caf6c6480fbf3ff1e5f0f184f739b2956
nilssen98 <kasper.nilssen1@gmail.com>
2024-11-16 14:08:35 +0100

    fix some steam issues

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -1,12 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, nixpkgs, ... }:
 {
   # Pipewire config
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+  services = {
+    pipewire = {
+      enable = true;
+      audio.enable = true;
+      pulse.enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      jack.enable = true;
+    };
   };
 }
--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -1,6 +1,19 @@
+{ pkgs, ... }:
 {
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraEnv = {
+        MANGOHUD = true;
+        OBS_VKCAPTURE = true;
+        RADV_TEX_ANISO = 16;
+      };
+      extraLibraries =
+        p: with p; [
+          atk
+        ];
+    };
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
