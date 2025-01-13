2f54ce59a7a0a01f80d7c631273a1e665d203556
nilssen98 <kasper.nilssen1@gmail.com>
2025-01-13 12:45:24 +0100

    add hyprland home manager module

---
+++ modules/home-manager/hyprland.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  lib,
+  config,
+}:
+{
+  options = {
+    hyprland.enable = lib.mkEnableOption "Enable Hyprland";
+  };
+
+  config = lib.mkIf config.hyprland.enable {
+    # Enable Hyprland
+    wayland.windowManager.hyprland.enable = true;
+
+    # Required for the default Hyprland config
+    programs.kitty.enable = true;
+
+    # Hint Electron apps to use Wayland:
+    home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+    # Programs don’t work in systemd services, but do on the terminal
+    wayland.windowManager.hyprland.systemd.variables = [ "--all" ];
+  };
+}
