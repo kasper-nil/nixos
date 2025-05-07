b282e9b169e133cd84b993674a73c96b0ee93672
nilssen98 <kasper.nilssen1@gmail.com>
2025-05-07 12:53:48 +0200

    attempt on fixing ttslabs prod electron stuff

--- shells/ttslabs-prod.nix
+++ shells/ttslabs-prod.nix
@@ -1,16 +1,30 @@
 { pkgs }:
+let
+  old-pkgs =
+    import
+      (fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz";
+        sha256 = "0v2z6jphhbk1ik7fqhlfnihcyff5np9wb3pv19j9qb9mpildx0cg";
+      })
+      {
+        system = builtins.currentSystem or "x86_64-linux"; # fallback for older versions
+      };
+in
 pkgs.mkShell {
-  nativeBuildInputs = [ pkgs.bashInteractive ];
-
-  buildInputs = with pkgs; [
-    nodejs_18
-    corepack_18
-    turbo
+  nativeBuildInputs = with pkgs; [
+    bashInteractive
+    old-pkgs.nodejs-18_x
+    old-pkgs.corepack_18
+    glib
+    glibc
+    gobject-introspection
+    nss
+    libnss-mysql
+    libnss_nis
   ];
 
   shellHook = ''
-    export TURBO_BINARY_PATH="${pkgs.turbo}/bin/turbo"
-
+    export LD_LIBRARY_PATH=${pkgs.glib.out}/lib:${pkgs.nss.lib}
     export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
     export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
     export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
