76ddea198ef3a4f35aacb5e4f6e543678f38f605
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-06-13 11:03:43 +0200

    revert to the previous hardware-config setup, as gitignore makes it not see this file

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -8,7 +8,7 @@
   imports = [
     inputs.home-manager.nixosModules.home-manager
     ../../modules/nixos
-    ../../hardware-configuration.nix
+    ./hardware-configuration.nix
   ];
 
   home-manager = {
