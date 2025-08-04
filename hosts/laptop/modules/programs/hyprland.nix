b9996eb7b4b7c99ec7ec59fb106e2fa449e4e9bf
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-08-04 18:42:15 +0200

    remove wrong config

--- hosts/laptop/modules/programs/hyprland.nix
+++ hosts/laptop/modules/programs/hyprland.nix
@@ -2,12 +2,5 @@
 {
   programs.hyprland = {
     enable = true;
-    extraConfig = ''
-      misc  { vfr = true }        # variable frame-rate
-      decoration {
-        blur { enabled = false }
-        drop_shadow = false
-      }
-    '';
   };
 }
