7bb23c5091cd7fb94b19ac8b82d24995263facd3
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-09-30 11:18:44 +0200

    refactor packages, add keymapp

--- hosts/desktop/modules/environment.nix
+++ hosts/desktop/modules/environment.nix
@@ -8,19 +8,14 @@
     systemPackages = with pkgs; [
       nixd
       nixfmt-rfc-style
-
       protonup-qt
       ffmpeg-full
-
-      # cli tools
       ranger
       htop
       tmux
       openvpn
       fastfetch
       xwayland-run
-
-      # software
       vscode
       libreoffice
       openrgb-with-all-plugins
@@ -30,37 +25,26 @@
       wdisplays
       pamixer
       pavucontrol
-
-      # Dotnet stuff
       dotnet-sdk_8
       dotnet-runtime_8
       csharpier
-
       # gaming
       prismlauncher
       lutris
-
       playerctl
       lact
       obsidian
       gh
       vlc
       godot
-
       logmein-hamachi
-
       easyeffects
-
       steamcmd
-
       woeusb-ng
-
       ntfs3g
-
-      osu-lazer-bin
-
       go
       gopls
+      keymapp
     ];
   };
 }
